0d63b06f533914516259ebf9aa2295e8b380a207
blankec <Komba-rus05@mail.ru>
2023-11-16 15:15:39 +0500

    labs 1

---
+++ labs/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace labs
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Prowerka();
+        }
+        public static void Prowerka()
+        {
+            List<float> numbers = new List<float>();
+            while (true)
+            {
+                Console.WriteLine("Введите число");
+                string str = Console.ReadLine();
+                if (str == "q" || str.Length == 0)
+                {
+                    break;
+                }
+                if (int.TryParse(str, out int intNumber))
+                {
+                    if (intNumber >= 0)
+                    {
+                        Console.WriteLine("+");
+                        numbers.Add(intNumber);
+                    }
+                    else
+                    {
+                        Console.WriteLine("-");
+                        numbers.Add(intNumber);
+                    }
+                }
+                else if (float.TryParse(str, out float floatNumber))
+                {
+                    if (floatNumber == numbers.Last())
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        numbers.Add(floatNumber);
+                    }
+                }
+            }
+        }
+    }
+}
